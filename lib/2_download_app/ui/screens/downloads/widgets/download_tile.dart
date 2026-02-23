1220220381d597aaa66bc17935b222c2baf41289
Khen chandarapisey <piseykhenchandara93@gmail.com>
2026-02-23 23:00:41 +0700

    TASK-002 - Downloader - Downloads Q1 – Create the download tiles

--- lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
+++ lib/2_download_app/ui/screens/downloads/widgets/download_tile.dart
@@ -1,18 +1,56 @@
 import 'package:flutter/material.dart';
- 
+
 import 'download_controler.dart';
 
 class DownloadTile extends StatelessWidget {
   const DownloadTile({super.key, required this.controller});
 
   final DownloadController controller;
- 
- // TODO
+
+  // TODO
 
   @override
   Widget build(BuildContext context) {
-    return Placeholder();
-     
-    // TODO
+    return ListenableBuilder(
+      listenable: controller,
+      builder: (context, child) {
+        return Card(
+          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          child: ListTile(
+            title: Text(controller.ressource.name),
+            subtitle: _buildSubtitle(),
+            trailing: _buildIcon(),
+          ),
+        );
+      },
+    );
+  }
+
+  // TODO
+  Widget? _buildSubtitle() {
+    if (controller.status == DownloadStatus.notDownloaded) return null;
+
+    final downloaded = (controller.progress * controller.ressource.size)
+        .toStringAsFixed(1);
+
+    final total = controller.ressource.size;
+
+    final percent = (controller.progress * 100).toStringAsFixed(1);
+
+    return Text("$percent completed - $downloaded of $total");
+  }
+
+  Widget? _buildIcon() {
+    switch (controller.status) {
+      case DownloadStatus.notDownloaded:
+        return IconButton(
+          icon: Icon(Icons.download),
+          onPressed: () => controller.startDownload(),
+        );
+      case DownloadStatus.downloading:
+        return Icon(Icons.autorenew);
+      case DownloadStatus.downloaded:
+        return Icon(Icons.folder);
+    }
   }
 }
